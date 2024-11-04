4707a5abcc4dcfae19d4ef577789c027ee402d9f
terumafu <119095669+terumafu@users.noreply.github.com>
2024-11-04 14:25:38 -0800

    added button :)

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 // todo
+const app: HTMLDivElement = document.querySelector("#app")!;
+const button = document.createElement("button");
+button.innerHTML = "click me!";
+button.addEventListener("click", () => {
+  alert("you clicked the button");
+});
+app.append(button);
